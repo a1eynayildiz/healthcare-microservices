adde148de0d71c7469c72a78fb622db3f47c702c
Aleyna Yıldız <aleynayildizc@gmail.com>
2026-06-26 00:00:23 +0300

    chore: rename Services folder to Microservices

--- Microservices/Doccure.BranchService/Program.cs
+++ Microservices/Doccure.BranchService/Program.cs
@@ -1,19 +1,6 @@
-using Doccure.BranchService.Settings;
-using Microsoft.Extensions.Options;
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-
-// Bu iki satır şunu yapar:
-// 1) appsettings.json'daki "DatabaseSettings" bölümünü DatabaseSettings sınıfına bağlar
-// 2) IDatabaseSettings istendiğinde DatabaseSettings'in dolu halini singleton olarak verir
-
-builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettingsKey"));
-builder.Services.AddSingleton<IDatabaseSettings>(sp => sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
-
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
